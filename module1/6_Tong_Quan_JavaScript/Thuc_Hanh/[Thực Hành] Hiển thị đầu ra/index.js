55e4b5ebb0cf14f57d096703185faa3a5847764b
Minh Nhat <tranvanminhnhat99@gmail.com>
2021-05-11 23:04:54 +0700

    [Thuc Hanh] Hien thi dau ra

---
+++ module1/6_Tong_Quan_JavaScript/Thuc_Hanh/[Thực Hành] Hiển thị đầu ra/index.js
@@ -0,0 +1,15 @@
+function B1showMessage() {
+    alert("Hello!!!");
+}
+
+function B2showMessage() {
+    document.getElementById('messageDiv').innerHTML = 'Xin Chao!';
+}
+
+function B3showMessage() {
+    document.write('Hi!');
+}
+
+function B4showMessage() {
+    console.log('Chao ban!!!'); // Co the xem duoc ket qua tren mang hinh console
+}
