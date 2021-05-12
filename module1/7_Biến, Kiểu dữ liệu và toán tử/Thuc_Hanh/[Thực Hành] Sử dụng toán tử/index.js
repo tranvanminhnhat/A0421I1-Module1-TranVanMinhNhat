b298f65dd754903b76e863cd697baae48dbb4099
Minh Nhat <tranvanminhnhat99@gmail.com>
2021-05-12 20:04:28 +0700

    [Thực Hành] Sử dụng toán tử

---
+++ module1/7_Biến, Kiểu dữ liệu và toán tử/Thuc_Hanh/[Thực Hành] Sử dụng toán tử/index.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+let width = parseInt(inputWidth); //parseInt de ep kieu chuoi thanh kieu so nguyen
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("the area is: "+ area);
