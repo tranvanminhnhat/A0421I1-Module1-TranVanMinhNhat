2cf2241fc1e96f444ab99e8a9b59ccc39ec5f1ef
Minh Nhat <tranvanminhnhat99@gmail.com>
2021-07-13 21:07:15 +0700

    Introduction to Java

---
+++ module2/src/b01_introduction_to_java/thuc_hanh/thoi_gian_cua_he_thong/SystemTime.java
@@ -0,0 +1,10 @@
+package b01_introduction_to_java.thuc_hanh.thoi_gian_cua_he_thong;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("It is: " + now);
+    }
+}
