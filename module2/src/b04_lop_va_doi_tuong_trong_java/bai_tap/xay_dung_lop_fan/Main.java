48fc0a0389bbc697f27ce0e2241ff9070f409536
Minh Nhat <tranvanminhnhat99@gmail.com>
2021-07-25 14:33:49 +0700

    [Bài tập] Lớp Fan

---
+++ module2/src/b04_lop_va_doi_tuong_trong_java/bai_tap/xay_dung_lop_fan/Main.java
@@ -0,0 +1,9 @@
+package b04_lop_va_doi_tuong_trong_java.bai_tap.xay_dung_lop_fan;
+
+public class Main {
+    public static void main(String[] args) {
+        Fan fan = new Fan();
+        fan.setOn(true);
+        System.out.println(fan.toString());
+    }
+}
